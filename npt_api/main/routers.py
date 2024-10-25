123b7d0ee8e3f8e614c07eae29fbf67ae19a2d38
ecalasans <ericcalasans@gmail.com>
2024-10-25 05:49:17 -0300

    Refresh token working

--- npt_api/main/routers.py
+++ npt_api/main/routers.py
@@ -1,12 +1,14 @@
 from rest_framework.routers import SimpleRouter
 
+from main.auth.viewsets import LoginViewSet, RefreshViewSet, RegisterViewSet
 from main.medico.viewsets import MedicoViewSet
-from main.auth.viewsets import RegisterViewSet
 
 router = SimpleRouter()
 
 router.register(r'medicos', MedicoViewSet, basename='medico')
 router.register(r'auth/register', RegisterViewSet, basename='register')
+router.register(r'auth/login', LoginViewSet, basename='login')
+router.register(r'auth/refresh', RefreshViewSet, basename='refresh')
 
 urlpatterns = [
     *router.urls
