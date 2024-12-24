5f2f3e1fb08a35bc9f1b229da504188aab4b0924
ecalasans <ericcalasans@gmail.com>
2024-12-24 17:52:42 -0300

    Update HospitalViewset - show patients by hospital

--- npt_api/main/hospital/viewsets/viewsets.py
+++ npt_api/main/hospital/viewsets/viewsets.py
@@ -1,8 +1,15 @@
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny
+from rest_framework.decorators import action
 
 from main.hospital.models import Hospital
 from main.hospital.serializers import HospitalSerializer
+
+from main.paciente.models import Paciente
+from main.paciente.serializers import PacienteSerializer
+
+from django.shortcuts import get_object_or_404
 
 class HospitalViewSet(ModelViewSet):
     serializer_class = HospitalSerializer
@@ -11,3 +18,10 @@ class HospitalViewSet(ModelViewSet):
 
     def get_queryset(self):
         return Hospital.objects.filter(is_active=True)
+
+    @action(detail=False, methods=['get'], url_path=r'(?P<sigla>[^/.]+)/pacientes')
+    def pacientes(self, request, sigla=None):
+        hospital = get_object_or_404(Hospital, sigla=sigla)
+        pacientes = Paciente.objects.filter(cod_hospital=hospital)
+        serializer  = PacienteSerializer(pacientes, many=True)
+        return Response(serializer.data)
